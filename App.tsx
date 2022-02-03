101d3f44f6103fc78a5de822a6196e457dc75488
Chris Kramer <kramerch@MC-C07TH2ACG1HW.local>
2022-02-03 10:03:05 -0600

    Initial commit

--- App.tsx
+++ App.tsx
@@ -1,11 +1,54 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from "react";
+import { Button, StyleSheet, Text, View } from "react-native";
+import Voice, {
+  SpeechResultsEvent,
+  SpeechErrorEvent,
+} from "@react-native-voice/voice";
 
 export default function App() {
+  const [results, setResults] = useState([] as string[]);
+  const [isListening, setIsListening] = useState(false);
+
+  useEffect(() => {
+    function onSpeechResults(e: SpeechResultsEvent) {
+      setResults(e.value ?? []);
+    }
+    function onSpeechError(e: SpeechErrorEvent) {
+      console.error(e);
+    }
+    Voice.onSpeechError = onSpeechError;
+    Voice.onSpeechResults = onSpeechResults;
+    return function cleanup() {
+      Voice.destroy().then(Voice.removeAllListeners);
+    };
+  }, []);
+
+  async function toggleListening() {
+    try {
+      if (isListening) {
+        await Voice.stop();
+        setIsListening(false);
+      } else {
+        setResults([]);
+        await Voice.start("en-US");
+        setIsListening(true);
+      }
+    } catch (e) {
+      console.error(e);
+    }
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Press the button and start speaking.</Text>
+      <Button
+        title={isListening ? "Stop Recognizing" : "Start Recognizing"}
+        onPress={toggleListening}
+      />
+      <Text>Results:</Text>
+      {results.map((result, index) => {
+        return <Text key={`result-${index}`}>{result}</Text>;
+      })}
     </View>
   );
 }
@@ -13,8 +56,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#F5FCFF",
   },
 });
